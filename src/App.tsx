23b9c22e7bf7fae45195a4d04b3d51a75ea1c25b
neiljoseph115-lab <neiljoseph115@gmail.com>
2026-06-04 22:17:21 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import Navbar from './components/home/Navbar';
-import Hero from './components/home/Hero';
-
 export default function App() {
   return (
-    <div className="bg-black text-white min-h-screen">
-      <Navbar />
-      <Hero />
+    <div style={{ padding: 40, color: 'black' }}>
+      Hello Neil
     </div>
   );
 }
