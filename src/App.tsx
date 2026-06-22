83a1e8ed52934ee754ac82e3b262c03d9e2c34d5
neiljoseph115-lab <neiljoseph115@gmail.com>
2026-06-22 18:18:26 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ const products = [
     name: 'PUMA LaFrancé Purple',
     category: 'Sneakers',
     price: '$140',
-    image: '/IMG_6427.jpeg',
+    image: 'https://raw.githubusercontent.com/neiljoseph115-lab/Neil/main/IMG_6427.jpeg',
   },
 
   {
