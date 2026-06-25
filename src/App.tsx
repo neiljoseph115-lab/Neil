54666d3d916e6eb0ae7512927289ca88e0df0755
neiljoseph115-lab <neiljoseph115@gmail.com>
2026-06-25 01:16:22 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,56 +5,50 @@ const products = [
     price: '$120',
     image: '/products/IMG_6425.jpeg',
   },
-
   {
     name: 'Jordan 4 Black White',
     category: 'Sneakers',
     price: '$220',
     image: '/products/jordan-4-black-white.jpg',
   },
-
   {
     name: 'PUMA LaFrancé Purple',
     category: 'Sneakers',
     price: '$140',
     image: 'https://raw.githubusercontent.com/neiljoseph115-lab/Neil/main/IMG_6427.jpeg',
   },
-
   {
     name: 'Affliction Octopus Tee',
     category: 'Apparel',
     price: '$65',
-    image: '/products/affliction-octopus.jpg',
+    image: 'https://raw.githubusercontent.com/neiljoseph115-lab/Neil/main/IMG_6427.jpeg',
   },
-
   {
     name: 'Memories Vintage Tee',
     category: 'Apparel',
     price: '$55',
-    image: '/products/memories-tee.jpg',
+    image: 'https://raw.githubusercontent.com/neiljoseph115-lab/Neil/main/IMG_6427.jpeg',
   },
-
   {
     name: 'Skeleton Graphic Tee',
     category: 'Apparel',
     price: '$60',
-    image: '/products/skeleton-tee.jpg',
+    image: 'https://raw.githubusercontent.com/neiljoseph115-lab/Neil/main/IMG_6427.jpeg',
   },
-
   {
     name: 'Paper Chaser Tee',
     category: 'Apparel',
     price: '$55',
-    image: '/products/paper-chaser-tee.jpg',
+    image: 'https://raw.githubusercontent.com/neiljoseph115-lab/Neil/main/IMG_6427.jpeg',
   },
-
   {
     name: 'Smile Through The Pain Tee',
     category: 'Apparel',
     price: '$55',
-    image: '/products/smile-through-pain-tee.jpg',
+    image: 'https://raw.githubusercontent.com/neiljoseph115-lab/Neil/main/IMG_6427.jpeg',
   },
-]
+];
+
 function App() {
   return (
     <div
