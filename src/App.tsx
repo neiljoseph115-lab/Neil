c195edb3e78175ee854e57c27cbca83315dca2e0
neiljoseph115-lab <neiljoseph115@gmail.com>
2026-06-26 14:54:11 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -18,29 +18,28 @@ const products = [
     image: '/products/IMG_6419.jpeg',
   },
   {
-    {
-  name: 'Affliction Octopus Tee',
-  category: 'Apparel',
-  price: '$65',
-  image: '/products/IMG_6429.webp',
-},
-{
-  name: 'Memories Vintage Tee',
-  category: 'Apparel',
-  price: '$55',
-  image: '/products/IMG_6428.jpeg',
-},
-{
-  name: 'Skeleton Graphic Tee',
-  category: 'Apparel',
-  price: '$60',
-  image: '/products/IMG_6432.webp',
-},
-{
-  name: 'Paper Chaser Tee',
-  category: 'Apparel',
-  price: '$55',
-  image: '/products/IMG_6431.webp',
+    name: 'Affliction Octopus Tee',
+    category: 'Apparel',
+    price: '$65',
+    image: '/products/IMG_6429.webp',
+  },
+  {
+    name: 'Memories Vintage Tee',
+    category: 'Apparel',
+    price: '$55',
+    image: '/products/IMG_6417.jpeg',
+  },
+  {
+    name: 'Skeleton Graphic Tee',
+    category: 'Apparel',
+    price: '$60',
+    image: '/products/IMG_6432.webp',
+  },
+  {
+    name: 'Paper Chaser Tee',
+    category: 'Apparel',
+    price: '$55',
+    image: '/products/IMG_6431.webp',
   },
 ];
 
