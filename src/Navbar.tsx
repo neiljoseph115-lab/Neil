343ebecb4b7931f4f3f453c9f211e0b0791bdda2
neiljoseph115-lab <neiljoseph115@gmail.com>
2026-06-27 22:06:58 -0400

    Update Navbar.tsx

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -30,7 +30,7 @@ export default function Navbar() {
             </button>
           </div>
 
-          <div className="flex flex-col gap-8 text-2xl font-medium uppercase tracking-[5px]">
+          <div className="flex flex-col gap-7 text-2xl font-medium uppercase tracking-[5px]">
             <a href="#">Shop</a>
             <a href="#">Collections</a>
             <a href="#">Our Story</a>
